4fb0375719f2441b5ee920814abaeeaf411a65c0
Chiyoungjun <happyboy1269@naver.com>
2024-10-09 00:24:18 +0900

    버전 맞추기

--- src/App.js
+++ src/App.js
@@ -1,48 +1,23 @@
-import React, { useState } from 'react';
-import { useNavigate, Routes, Route } from 'react-router-dom';
-import head from "./image/head.png";
-import main from './image/main.png';
-import Nav from "./components/Nav";
-import Mypage from './page/mypage';
-import MainBody from './page/mainBody';
-import Login from './page/Login';
+import logo from './logo.svg';
 import './App.css';
- 
-function App() {
-  const navigate = useNavigate();
-  const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
 
-  const openLoginModal = () => setIsLoginModalOpen(true);
-  const closeLoginModal = () => setIsLoginModalOpen(false);
- 
+function App() {
   return (
-    //예제 연습
     <div className="App">
-      <header className="app-header">
-        <div className="header-content">
-          <h1>
-            <img 
-              src={head} 
-              alt="head icon"
-              style={{ cursor: "pointer" }}
-              onClick={() => navigate('/')}
-            />
-          </h1>
-        </div>
-        <button className="login-button" onClick={openLoginModal}>로그인</button>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
-      <Nav />
-      <div className='main-image'>
-        <img src={main} alt="main icon"/>
-      </div>
-      <main>
-        <MainBody/>
-      </main>
-      <Routes>
-        <Route path="/mypage" element={<Mypage />} />
-        {/* 여기에 다른 라우트들을 추가하세요 */}
-      </Routes>
-      <Login isOpen={isLoginModalOpen} onClose={closeLoginModal} />
     </div>
   );
 }
