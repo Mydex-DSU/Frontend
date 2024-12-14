e3d19a5986bf81b071bb04c88c9584efeaafcefd
Chiyoungjun <happyboy1269@naver.com>
2024-12-14 17:54:19 +0900

    feat: 자잘한 수정

--- src/adm_f/component/Mydex/Mydex.js
+++ src/adm_f/component/Mydex/Mydex.js
@@ -1,12 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './mydex.css';
 import MydexModal from '../MydexModal/MydexModal';
+import axios from 'axios';
 
 const Mydex = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [scholarshipData, setScholarshipData] = useState(null);
+
+  useEffect(() => {
+    const fetchScholarshipData = async () => {
+      try {
+        const response = await axios.get('http://100.94.142.127:3000/mydexscholarship');
+        const sortedData = response.data.mydexscholarship.sort((a, b) => 
+          b.mydex_scholarship_application_period_id - a.mydex_scholarship_application_period_id
+        );
+        setScholarshipData(sortedData[0]);
+      } catch (error) {
+        console.error('Error fetching scholarship data:', error);
+      }
+    };
+
+    fetchScholarshipData();
+  }, []);
+
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    return date.toLocaleDateString('ko-KR', { year: 'numeric', month: '2-digit', day: '2-digit', weekday: 'short' });
+  };
 
   return (
-    
     <div>
       <h2 className='adm_MydexHead'>Mydex 온도 포인트 장학금 신청 기간 등록</h2>
       <div className="adm_content-box">
@@ -16,13 +38,15 @@ const Mydex = () => {
         <p className="adm_text">
           등록 기간은 학기 종강일에 시작해야하며 기간은 일주일로 고정됩니다.
         </p>
-        <p className="adm_text">
-          이번 학기 종강일은 2024-12-20(금) 입니다.
-        </p>
+        {scholarshipData && (
+          <p className="adm_text">
+            이번 학기 종강일은 {formatDate(scholarshipData.mydex_application_start_dateTime)} 입니다.
+          </p>
+        )}
       </div>
       <button className="adm_register-button" onClick={() => setIsModalOpen(true)}>
-          등록
-        </button>
+        등록
+      </button>
       <MydexModal 
         isOpen={isModalOpen} 
         onClose={() => setIsModalOpen(false)} 
--- src/adm_f/component/admNav/admNav.js
+++ src/adm_f/component/admNav/admNav.js
@@ -1,9 +1,12 @@
 import React from "react";
 import'./admNav.css';
+import { Link } from "react-router-dom";
 
 const Nav = () => {
     return(
-    <div className="adm-mydex-header">MYDEX</div>
+        <div className="adm-mydex-header">
+            <Link to="/adm/admmainpage">MYDEX</Link>
+        </div>
     )
 }
 
